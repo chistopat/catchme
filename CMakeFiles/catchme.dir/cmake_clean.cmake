dfca5c6dbf5e06f067ae8c2cebe188da03dff5b1
chistopat <chistopat@yandex.ru>
2022-10-27 18:47:34 +0000

    pack

---
+++ CMakeFiles/catchme.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/catchme.dir/main.cpp.o"
+  "CMakeFiles/catchme.dir/main.cpp.o.d"
+  "catchme"
+  "catchme.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/catchme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/catchme.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for catchme.
+# This may be replaced when dependencies are built.
